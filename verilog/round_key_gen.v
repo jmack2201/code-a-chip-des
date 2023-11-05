e0888d388eac9826a83c81f8793de6eabda8b203
Jacob Mack <jmack39@gatech.edu>
2023-11-05 16:50:49 -0500

    Fixed shifting issue when generating round keys

--- verilog/round_key_gen.v
+++ verilog/round_key_gen.v
@@ -10,74 +10,21 @@ module round_key_gen #(
     reg [27:0] input_key_left_shifted;
     reg [27:0] input_key_right_shifted;
     reg [55:0] input_key_combined;
-    reg left_MSB;
-    reg right_MSB;
-    integer i;
     always @(*) begin
         input_key_left_shifted=key_input[55:28];
         input_key_right_shifted=key_input[27:0];
         case (round_num)
             0,1,8,15:begin  //shift once
-                //preserve MSB
-                left_MSB = input_key_left_shifted[27];
-                right_MSB = input_key_right_shifted[27];
-                for (i = 27; i<0; i=i-1) begin
-                    if (i == 0) begin //at the end, the old MSB becomes the new LSB
-                        input_key_left_shifted[0] = left_MSB;
-                        input_key_right_shifted[0] = right_MSB;
-                    end else begin //otherwise everything shifts left
-                        input_key_left_shifted[i] = input_key_left_shifted[i-1];
-                        input_key_right_shifted[i] = input_key_right_shifted[i-1];
-                    end
-                end
+                input_key_left_shifted = {input_key_right_shifted[26:0],input_key_right_shifted[27]};
+                input_key_right_shifted = {input_key_right_shifted[26:0],input_key_right_shifted[27]};
             end 
             2,3,4,5,6,7,9,10,11,12,13,14:begin //shift twice
-                left_MSB = input_key_left_shifted[27];
-                right_MSB = input_key_right_shifted[27];
-                for (i = 27; i<0; i=i-1) begin
-                    if (i == 0) begin //at the end, the old MSB becomes the new LSB
-                        input_key_left_shifted[0] = left_MSB;
-                        input_key_right_shifted[0] = right_MSB;
-                    end else begin //otherwise everything shifts left
-                        input_key_left_shifted[i] = input_key_left_shifted[i-1];
-                        input_key_right_shifted[i] = input_key_right_shifted[i-1];
-                    end
-                end
-                left_MSB = input_key_left_shifted[27];
-                right_MSB = input_key_right_shifted[27];
-                for (i = 27; i<0; i=i-1) begin
-                    if (i == 0) begin //at the end, the old MSB becomes the new LSB
-                        input_key_left_shifted[0] = left_MSB;
-                        input_key_right_shifted[0] = right_MSB;
-                    end else begin //otherwise everything shifts left
-                        input_key_left_shifted[i] = input_key_left_shifted[i-1];
-                        input_key_right_shifted[i] = input_key_right_shifted[i-1];
-                    end
-                end
+                input_key_left_shifted = {input_key_right_shifted[25:0],input_key_right_shifted[27:26]};
+                input_key_right_shifted = {input_key_right_shifted[25:0],input_key_right_shifted[27:26]};
             end
             default: begin //shift twice
-                left_MSB = input_key_left_shifted[27];
-                right_MSB = input_key_right_shifted[27];
-                for (i = 27; i<0; i=i-1) begin
-                    if (i == 0) begin //at the end, the old MSB becomes the new LSB
-                        input_key_left_shifted[0] = left_MSB;
-                        input_key_right_shifted[0] = right_MSB;
-                    end else begin //otherwise everything shifts left
-                        input_key_left_shifted[i] = input_key_left_shifted[i-1];
-                        input_key_right_shifted[i] = input_key_right_shifted[i-1];
-                    end
-                end
-                left_MSB = input_key_left_shifted[27];
-                right_MSB = input_key_right_shifted[27];
-                for (i = 27; i<0; i=i-1) begin
-                    if (i == 0) begin //at the end, the old MSB becomes the new LSB
-                        input_key_left_shifted[0] = left_MSB;
-                        input_key_right_shifted[0] = right_MSB;
-                    end else begin //otherwise everything shifts left
-                        input_key_left_shifted[i] = input_key_left_shifted[i-1];
-                        input_key_right_shifted[i] = input_key_right_shifted[i-1];
-                    end
-                end
+                input_key_left_shifted = {input_key_right_shifted[25:0],input_key_right_shifted[27:26]};
+                input_key_right_shifted = {input_key_right_shifted[25:0],input_key_right_shifted[27:26]};
             end
         endcase
         input_key_combined[55:28] = input_key_left_shifted;
