3ac1b15897e96cefc4a0a3b82799777334c92ab3
Jacob Mack <jmack39@gatech.edu>
2023-11-12 21:01:23 -0500

    Fixed for loop in key_mux

--- verilog/src/key_mux.v
+++ verilog/src/key_mux.v
@@ -2,11 +2,13 @@
 module key_mux (
     input encrypt_decrypt,
     input [0:15][47:0] round_keys_i,
-    output [0:15][47:0] round_keys_o
+    output reg [0:15][47:0] round_keys_o
 );
     integer i;
-    for (i = 0; i < 16; i = i +1) begin
-        assign round_keys_o[i] = !encrypt_decrypt ? round_keys_i[i] : round_keys_i[15-i];
+    always @(*) begin    
+        for (i = 0; i < 16; i = i +1) begin
+            round_keys_o[i] = !encrypt_decrypt ? round_keys_i[i] : round_keys_i[15-i];
+        end
     end
-
+    
 endmodule
