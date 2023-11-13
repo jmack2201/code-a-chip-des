aba2aa99c74d766ea14e148f06db7037efb9adc5
Jacob Mack <jmack39@gatech.edu>
2023-11-13 00:17:38 -0500

    remove unused port

--- verilog/src/des_top.v
+++ verilog/src/des_top.v
@@ -15,7 +15,7 @@ module des_top (
 
     key_gen key_gen(.init_key(cipher_key), .round_keys(round_keys_output), .encrypt_decrypt(encrypt_decrypt));
 
-    round_stack round_stack (.init_perm_plain_text(init_perm_out), .round_keys(round_keys_output), .encrypt_decrypt(encrypt_decrypt), .plain_text_final_perm(final_perm_in));
+    round_stack round_stack (.init_perm_plain_text(init_perm_out), .round_keys(round_keys_output), .plain_text_final_perm(final_perm_in));
 
     final_perm final_perm (.final_p_box_i(final_perm_in), .final_p_box_o(cipher_text));
     
