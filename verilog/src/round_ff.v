17d699d9161c5b238809c43bacb810ea141222ae
Jacob Mack <jmack39@gatech.edu>
2023-11-13 01:06:57 -0500

    Created round ff

---
+++ verilog/src/round_ff.v
@@ -0,0 +1,17 @@
+module round_ff (
+    input clk, rstn,
+    input valid_i,
+    input [63:0] round_in,
+    output reg valid_o,
+    output reg [63:0] round_out
+);
+    always @(posedge clk ) begin
+        if (!rstn) begin
+            round_out <= 0;
+            valid_o <= 0;
+        end else begin
+            round_out <= round_in;
+            valid_o <= valid_i;
+        end
+    end
+endmodule
