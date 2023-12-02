7dd61ab8db16b48534a10e92db88bb48d29da789
Jacob Mack <jmack39@gatech.edu>
2023-12-02 00:06:50 -0500

    Added config parameter for FF on key_gen output

--- verilog/src/des_config.v
+++ verilog/src/des_config.v
@@ -1,4 +1,5 @@
 `ifndef des_config
-    `define NUM_ROUND_STAGES_FF 1
-    `define NUM_KEY_STAGES_FF 1
+    `define NUM_ROUND_STAGES_FF 8
+    `define NUM_KEY_STAGES_FF 0
+    `define ROUND_KEY_OUT_FF 1
 `endif
